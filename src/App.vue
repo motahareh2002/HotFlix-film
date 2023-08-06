f955173abcb9081a7ee25fcac86ba952d10b0ffa
motahareh jafarian <motaharehjafi@gmail.com>
2023-08-06 21:47:31 +0330

    change

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,9 @@
 <script setup lang="ts">
-import HomeView from './views/HomeView.vue';
-
 </script>
 
 <template>
   <div>
-    <HomeView />
+    <router-view />
   </div>
 </template>
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,6 +4,9 @@ import Menu from "../components/menus/Menu.vue";
 </script>
 <template>
   <div class="selection:bg-[#f9ab00] selection:text-[#FFFFFF]">
-    <router-view />
+    <router-link :to="{path: '/'}">
+      <MobileMenu />
+      <Menu />
+    </router-link>
   </div>
 </template>
