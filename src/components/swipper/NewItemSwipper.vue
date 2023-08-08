e2da54bcc29655029f864a137662e214bd17b469
motahareh jafarian <motaharehjafi@gmail.com>
2023-08-08 22:53:18 +0330

    add background in swipper

--- src/components/swipper/NewItemSwipper.vue
+++ src/components/swipper/NewItemSwipper.vue
@@ -5,37 +5,34 @@ import ArrowRight from "../icons/ArrowRight.vue";
 import { ref } from "vue";
 import type { Ref } from "vue";
 let backImg = [
-  { id: 1, src: "../assets/img/home__bg.jpg" },
-  { id: 2, src: "../assets/img/home__bg2.jpg" },
-  { id: 3, src: "../assets/img/home__bg3.jpg" },
-  { id: 4, src: "../assets/img/home__bg4.jpg" },
-  { id: 5, src: "../assets/img/home__bg5.jpg" },
+  { id: 1, src: "http://hotflix.volkovdesign.com/main/img/home/home__bg.jpg" },
+  { id: 2, src: "http://hotflix.volkovdesign.com/main/img/home/home__bg2.jpg" },
+  { id: 3, src: "http://hotflix.volkovdesign.com/main/img/home/home__bg3.jpg" },
+  { id: 5, src: "http://hotflix.volkovdesign.com/main/img/home/home__bg5.jpg" },
 ];
 const index: Ref<number> = ref(0);
-const x: Ref<string> = ref(
-  "../assets/img/home__bg.jpg"
+const nextTo: Ref<number> = ref(0);
+const backgroundImg: Ref<string> = ref(
+  "http://hotflix.volkovdesign.com/main/img/home/home__bg.jpg"
 );
 function nextItem() {
   if (index.value === backImg.length) index.value = 0;
-  const nextTo: Ref<number> = ref(0);
   nextTo.value = index.value++;
-  x.value = backImg[nextTo.value].src;
+  backgroundImg.value = backImg[nextTo.value].src;
 }
 function prevITem() {
   if (index.value === 0) index.value = backImg.length - 1;
-  const nextTo: Ref<number> = ref(0);
   nextTo.value = index.value--;
-  x.value = backImg[nextTo.value].src;
+  backgroundImg.value = backImg[nextTo.value].src;
 }
-console.log(backImg.length);
-
 </script>
 
 <template>
-  <div :class="`bg-[url('${x}')] p-10 text-white`">
+  <div class=" text-white bg-blend-darken px-6 py-14" 
+      :style="{ background: `rgba(0,0,0,0.7) url(${backgroundImg})`}">
     <div class="flex justify-between">
-      <div class="text-xl">
-        <span class="font-bold">NEW ITEMS </span>
+      <div class="text-2xl leading-relaxed ">
+        <span class="font-black">NEW ITEMS </span>
         <span>OF THIS SEASON</span>
       </div>
       <div class="flex justify-between text-xl opacity-90">
@@ -46,10 +43,5 @@ console.log(backImg.length);
     <div></div>
   </div>
 </template>
-<!-- <style>
-    div{
-        background-image: url('');
-    }
-</style> -->
 
 
