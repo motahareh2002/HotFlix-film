e11682508c685c6bc3b275bac57e3da3ad598562
motahareh jafarian <motaharehjafi@gmail.com>
2023-08-16 09:02:30 +0330

    add footer

---
+++ src/components/footer/FooterPage.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="text-white border-t px-3">
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Unde, sed
+    cupiditate quas dignissimos beatae, aliquid repellendus rem quisquam
+    repudiandae illo ad. Ea ducimus officiis modi repellendus itaque, eius nobis
+    quibusdam magni sapiente cumque, atque iure sequi natus. Voluptates pariatur
+    fuga quis, incidunt repellat explicabo ea. Eveniet sequi obcaecati vel quis
+    quaerat illo omnis, iste corrupti consequuntur commodi est dolores quasi
+    autem saepe fugit sit beatae assumenda velit quisquam dolore ea, voluptatem
+    tempora. Dignissimos dolor ipsum debitis perspiciatis obcaecati dolore aut
+    odio natus. Voluptates inventore minus iste quisquam eum velit error nostrum
+    mollitia earum saepe. Soluta nostrum dignissimos dolor accusantium porro?
+  </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
-import MobileMenu from "../components/menus/MobileMenu.vue";
-import Menu from "../components/menus/Menu.vue";
-import NewItemSwipper from "../components/swipper/NewItemSwipper.vue";
-import NewItems from "../components/newItems/NewItems.vue";
-import ExpectedPremiere from "../components/expectedPremiere/ExpectedPremiere.vue";
-import BestPlace from "../components/bestPlace/BestPlace.vue";
+import MobileMenu from "@/components/menus/MobileMenu.vue";
+import Menu from "@/components/menus/Menu.vue";
+import NewItemSwipper from "@/components/swipper/NewItemSwipper.vue";
+import NewItems from "@/components/newItems/NewItems.vue";
+import ExpectedPremiere from "@/components/expectedPremiere/ExpectedPremiere.vue";
+import BestPlace from "@/components/bestPlace/BestPlace.vue";
 import PricePlace from "@/components/bestPlace/PricePlace.vue";
+import FooterPage from "@/components/footer/FooterPage.vue"
 </script>
 <template>
   <div class="selection:bg-primary selection:text-[#FFFFFF]">
@@ -20,5 +21,6 @@ import PricePlace from "@/components/bestPlace/PricePlace.vue";
     <ExpectedPremiere />
     <best-place />
     <price-place />
+    <FooterPage />
   </div>
 </template>
